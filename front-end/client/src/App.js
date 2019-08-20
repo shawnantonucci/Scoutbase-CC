ccaa23f536d98259fa63d56ec2d77ec87617345c
shawn <shawnantonucci2018@gmail.com>
2019-08-20 16:59:00 -0700

    countries component consuming graphql api and displaying data

--- front-end/client/src/App.js
+++ front-end/client/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './App.css';
-import { Route} from "react-router-dom";
-import Links from "./components/Links"
-import Countries from "./components/Countries"
-import SingleCountry from "./components/SingleCountry"
+import React from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Links from "./components/Links";
+import Countries from "./components/Countries";
+import SingleCountry from "./components/SingleCountry";
 
 function App() {
-  return (
-    <div className="App">
-      <Route path="/" exact component={Links} />
-      <Route path="/countries" exact component={Countries} />
-      <Route path="/countries/:code" exact component={SingleCountry} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Route path="/" exact component={Links} />
+            <Route path="/countries" exact component={Countries} />
+            <Route path="/countries/:code" exact component={SingleCountry} />
+        </div>
+    );
 }
 
 export default App;
--- front-end/client/src/components/Countries.js
+++ front-end/client/src/components/Countries.js
@@ -1,13 +1,87 @@
-import React from 'react'
-import Links from './Links'
+import React from "react";
+import Links from "./Links";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import styled from "styled-components";
+
+const GET_COUNTRIES = gql`
+    {
+        countries {
+            name
+            code
+            languages {
+                name
+            }
+            continent {
+                name
+            }
+        }
+    }
+`;
+
+const Country = () => {
+    const { data, error, loading } = useQuery(GET_COUNTRIES);
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+    if (error) {
+        return <div>Error! {error.message}</div>;
+    }
+
+    return (
+        <CountryContainer>
+            {data.countries.map(country => (
+                <CountryCard key={country.code}>
+                    <li>
+                        <b>Continent: </b>
+                        {country.continent.name}
+                    </li>
+                    <li>
+                        <b>Country Name: </b>
+                        {country.name}
+                    </li>
+                    <b>Languages spoken:</b>
+                    {country.languages.map(language => {
+                        return (
+                            <li key={language.name}>{`${language.name},`}</li>
+                        );
+                    })}
+                </CountryCard>
+            ))}
+        </CountryContainer>
+    );
+};
 
 const Countries = () => {
     return (
         <div>
             <Links />
-            Countries
+            <Country />
         </div>
-    )
-}
+    );
+};
 
-export default Countries
+export default Countries;
+
+const CountryCard = styled.div`
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    width: 20%;
+    height: auto;
+    align-items: flex-start;
+    margin: 15px;
+    padding: 15px;
+
+    li {
+        list-style-type: none;
+    }
+`;
+
+const CountryContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    border: 1px solid black;
+    flex-flow: wrap;
+    width: 100%;
+`;
