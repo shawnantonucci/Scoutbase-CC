bef4436622c5807298a3bf446af682e863c97b00
shawn <shawnantonucci2018@gmail.com>
2019-08-20 15:40:26 -0700

    added a single country route with code params

--- front-end/client/src/App.js
+++ front-end/client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Route} from "react-router-dom";
 import Links from "./components/Links"
 import Countries from "./components/Countries"
+import SingleCountry from "./components/SingleCountry"
 
 function App() {
   return (
     <div className="App">
       <Route path="/" exact component={Links} />
       <Route path="/countries" exact component={Countries} />
+      <Route path="/countries/:code" exact component={SingleCountry} />
     </div>
   );
 }
---
+++ front-end/client/src/components/SingleCountry.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Links from './Links'
+
+const SingleCountry = () => {
+    return (
+        <div>
+            <Links />
+            Country #1
+        </div>
+    )
+}
+
+export default SingleCountry
